adaf9b6b61fb78c7503fefb301c61cc074891cbf
abhinav-dvp <83061620+abhinav-dvp@users.noreply.github.com>
2022-10-28 21:16:02 -0700

    Update Note windows.py

--- Note windows.py
+++ Note windows.py
@@ -2,20 +2,30 @@ import speech_recognition as sr
 import pyttsx3
 r = sr.Recognizer()
 def main():
-    with sr.Microphone() as mic:
-        f = open('note.txt','a+')
-        r.adjust_for_ambient_noise(mic,duration = 2)
-        audio = r.listen(mic)
-        text = r.recognize_google(audio)
-        text = text.lower()       
-        if 'stop' in text :
-            print("terminating")
-            #f.close()
-        else:
-            L = text + '.'+'\n'
-            f.write(L)
-            f.close()
-            main()
+    try:
+        
+        with sr.Microphone() as mic:
+            f = open('note.txt','a+')
+            r.adjust_for_ambient_noise(mic,duration = 2)
+            audio = r.listen(mic)
+            print("Listining")
+            text = r.recognize_google(audio)
+            text = text.lower()       
+            if 'stop' in text :
+                print("terminating")
+            else:
+                print ("Stop not found")
+                print("Writing down")
+                L = text + '.'+'\n'
+                f.write(L)
+                f.close()
+                print("running the program again)")
+                main()
+    except sr.UnknownValueError():
+
+        r = sr.Recognizer()
+        main()
 main()
+        
         
         
